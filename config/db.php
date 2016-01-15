cc3fd795514f83e7b4c1577a20faa07468312aeb
Sowander <sowander1986@gmail.com>
2016-01-15 11:28:27 +0600

    изучение основ yii2 создание форм предстовлений вьюверов действий и тд

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'sqlite:/../web/rss/news.db',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
